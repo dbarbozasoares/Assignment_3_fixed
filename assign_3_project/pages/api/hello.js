10c8e4004c91b96d6bd749c715514f640e569fdf
dbarbozasoares <dbarboza.soares@gmail.com>
2025-03-18 20:50:26 -0400

    ready to go

--- assign_3_project/pages/api/hello.js
+++ assign_3_project/pages/api/hello.js
@@ -1,3 +1,13 @@
+/********************************************************************************
+ * WEB422 – Test 04
+ *
+ * I declare that this assignment is my own work in accordance with Seneca's
+ * Academic Integrity Policy:
+ * https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+ *
+ * Name: Diego B Soares Student ID: ____145820239____ Date: __mar - 18 - 2025_______
+ * Published URL: _________________https://github.com/dbarbozasoares/Assignment_3_fixed__________________________________________
+ ********************************************************************************/
 export default function handler(req, res) {
   res.status(200).json({ name: "John Doe" });
 }
